83d7f737759e12a5b1707fa3965af2b125811ff6
Bertrand <mutanganabertrand@gmail.com>
2022-11-12 19:42:55 +0200

    Add styles

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,17 +4,20 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { FaUser } from 'react-icons/fa';
 
 const drawerWidth = 300;
 const TopBar = ({ handleDrawerToggle }) => (
   <Box sx={{ display: 'flex' }}>
     <AppBar
+      color="transparent"
       position="fixed"
       sx={{
         width: { sm: `calc(100% - ${drawerWidth}px)` },
         ml: { sm: `${drawerWidth}px` },
+        boxShadow: 'none',
       }}
     >
       <Toolbar>
@@ -27,9 +30,35 @@ const TopBar = ({ handleDrawerToggle }) => (
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" noWrap component="div">
-          Responsive drawer
-        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            width: '100%',
+            mr: 10,
+          }}
+        >
+          <div className="image-holder">
+            <img
+              src="https://countryflagsapi.com/png/in"
+              alt="logo"
+              style={{ width: '100%', borderRadius: '1rem' }}
+            />
+          </div>
+          <ArrowDropDownIcon />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <div className="user-round">
+            <FaUser className="icon" />
+          </div>
+        </Box>
       </Toolbar>
     </AppBar>
   </Box>
